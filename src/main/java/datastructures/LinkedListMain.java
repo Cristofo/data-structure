79f1f9059d69f5b194725309966c2688b746f818
Christopher <christopher.soto@externos.bci.cl>
2022-09-21 20:00:34 -0300

    linkedList

--- src/main/java/datastructures/LinkedListMain.java
+++ src/main/java/datastructures/LinkedListMain.java
@@ -11,16 +11,28 @@ public class LinkedListMain {
         Node newNode2 = new Node(24, null);
         linkedList.addNodeLast(newNode);
         linkedList.addNodeLast(newNode2);
-        Node newNode3 = new Node(17, null);
 
 
         linkedList.printLinkedList();
 
         System.out.println("#######################");
 
-        linkedList.addNodePosition(newNode3, 0);
+        linkedList.addNodePosition(13, 0);
+        linkedList.addNodePosition(32, 5);
 
+        System.out.println("Added a Node");
         linkedList.printLinkedList();
+
+        linkedList.deleteNodePosition(4);
+
+        System.out.println("Deleting a Node");
+        linkedList.printLinkedList();
+
+        System.out.println("Deleting a Node by key");
+        linkedList.deleteByValue(19);
+        linkedList.printLinkedList();
+
+
     }
 
 }
@@ -32,26 +44,26 @@ class LinkedList {
         this.head = head;
     }
 
-    public void addNodePosition(Node node, int position){
-        if(isEmpty() || position > getSize()){
+    public void addNodePosition(int data, int position){
+        if(isEmpty() || isOutOfRangeAdd(position)){
             System.out.println("Not found");
         }else {
             Node previous = null;
             Node current = head;
-            Node next = current.nextNode;
-
+            Node next = current.getNextNode();
+            Node newNode = new Node(data, null);
             for(int i = 0; i <= position;i++){
 
                 if(i == position){
                     if(previous == null) {
-                        addNodeFirst(node);
+                        addNodeFirst(newNode);
                     } else{
-                        previous.setNextNode(node);
-                        node.setNextNode(current);
+                        previous.setNextNode(newNode);
+                        newNode.setNextNode(current);
                     }
                     break;
                 } else if(next == null){
-                    addNodeLast(node);
+                    addNodeLast(newNode);
                     break;
                 }
                 previous = current;
@@ -61,6 +73,75 @@ class LinkedList {
         }
     }
 
+    public void deleteNodePosition(int position){
+
+        if(isEmpty() || isOutOfRangeDelete(position)){
+            System.out.println("Not found");
+        }else {
+
+            Node previous = null;
+            Node current = head;
+            Node next = current.getNextNode();
+            for(int i = 0; i <= position; i++){
+                if(position == i){
+                    if(previous == null){
+                        head = next;
+                    } else if (next == null){
+                        previous.setNextNode(null);
+                    } else {
+                        previous.setNextNode(next);
+                    }
+                    break;
+                }
+                previous = current;
+                current = next;
+                next = next.getNextNode();
+            }
+        }
+    }
+
+
+    public void deleteByValue(int value){
+        if(isEmpty()){
+            System.out.println("Lista vacía");
+        }else {
+
+            Node current = head;
+            Node next = current.getNextNode();
+            if(current.getData() == value){
+
+                if(next != null){
+                    head = next;
+                }else{
+                    head = null;
+                }
+            }else{
+                while(next != null){
+                  if(next.getData() == value){
+                      Node newNext = next.getNextNode();
+                      current.setNextNode(newNext);
+                  }
+                  current = next;
+                  next = next.getNextNode();
+                }
+            }
+        }
+    }
+
+    public void reverseLinkedList(){
+
+
+    }
+
+
+
+    public boolean isOutOfRangeAdd(int position){
+        return position > getSize() || position < 0;
+    }
+
+    public boolean isOutOfRangeDelete(int position){
+        return position > getSize()-1 || position < 0;
+    }
     public boolean isEmpty(){
         return head == null;
     }
@@ -71,7 +152,7 @@ class LinkedList {
 
         while (current != null){
             i++;
-            current = current.nextNode;
+            current = current.getNextNode();
         }
         return i;
     }
@@ -84,36 +165,37 @@ class LinkedList {
     public void addNodeFirst(Node node){
        Node second = head;
        head = node;
-       head.nextNode = second;
+       head.setNextNode(second);
     }
 
 
     public Node getLast(){
 
         Node last = head;
-        while (last != null){
-            if(last.nextNode == null){
-                break;
+        if(last == null){
+            return null;
+        } else {
+            while (last.getNextNode() != null){
+                last = last.getNextNode();
             }
-            last = last.nextNode;
+            return last;
         }
 
-        return last;
     }
 
     public void printLinkedList(){
 
         Node last = head;
         while (last != null){
-            System.out.println("nro :"+ last.data);
-            last = last.nextNode;
+            System.out.println("nro :"+ last.getData());
+            last = last.getNextNode();
         }
     }
 }
 
 class Node {
-    int data;
-    Node nextNode;
+    private int data;
+    private Node nextNode;
 
     public Node(int data, Node nextNode) {
         this.data = data;
@@ -123,8 +205,15 @@ class Node {
     public Node getNextNode() {
         return nextNode;
     }
-
     public void setNextNode(Node nextNode) {
         this.nextNode = nextNode;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
     }
 }
